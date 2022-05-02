fad3ef411b6a809780f1d85109aefd0fd4358e0b
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2022-05-02 15:10:32 +0200

    Fix Problem in migration

--- db/migrate/20220501103537_add_timestamps_to_posts.rb
+++ db/migrate/20220501103537_add_timestamps_to_posts.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToPosts < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :created_at, :datetime
-    add_column :posts, :updated_at, :datetimes
+    add_column :posts, :updated_at, :datetime
   end
 end
