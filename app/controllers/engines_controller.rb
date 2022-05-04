d167d0c8d8ad169a71d0218d905e5849408e6c10
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2022-05-04 15:03:41 +0200

    Heroku fix

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -9,6 +9,7 @@ class EnginesController < ApplicationController
   def home; end
 
   def search
+    session[:user_id] = nil
     populate_db
     choice = params[:engine][:choice]
     search_value = params[:engine][:search_value]
